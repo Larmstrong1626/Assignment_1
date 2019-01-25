46a5bf90c9f744803594be5fc5184aa63344da82
Larmstrong1626 <rarmstrong7@my.apsu.edu>
2019-01-25 12:02:21 -0600

    Initial commit

---
+++ app/src/main/java/com/oc/assignment_1/MainActivity.java
@@ -0,0 +1,23 @@
+/*************
+ Developer: Robert Armstrong/Justin Duff
+ App: Assignment_1
+ MainActivity.java
+ Tested on Samsung Galaxy 5
+ 5.1"x2.85"
+ Marshmallow 6.0.1
+ API 23
+ 
+ **************/
+package com.oc.assignment_1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
