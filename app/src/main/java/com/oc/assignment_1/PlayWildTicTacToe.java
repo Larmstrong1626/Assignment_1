c409179f548e30af2444efbe6bba917e54492487
justin <justind417@gmail.com>
2019-01-26 12:39:35 -0600

    Added activity for Wild Tic Tac Toe. Edited Main activity to implement Wild activity. Created layout for Wild Tic Tac Toe.

---
+++ app/src/main/java/com/oc/assignment_1/PlayWildTicTacToe.java
@@ -0,0 +1,13 @@
+package com.oc.assignment_1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PlayWildTicTacToe extends Activity{
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wild);
+
+    }
+}
