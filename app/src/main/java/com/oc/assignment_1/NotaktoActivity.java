56b7a2699662a83a3d614fef29831886d51d8fa6
Larmstrong1626 <rarmstrong7@my.apsu.edu>
2019-01-26 11:33:07 -0600

    Added Notakto Activity and Layout

---
+++ app/src/main/java/com/oc/assignment_1/NotaktoActivity.java
@@ -0,0 +1,14 @@
+package com.oc.assignment_1;
+
+import android.app.Activity;
+
+import android.os.Bundle;
+
+public class NotaktoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notakto);
+    }
+}
