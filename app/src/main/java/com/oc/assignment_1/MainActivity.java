c46880226a1acb0d21f13ed229cdfe8c37685b56
Larmstrong1626 <rarmstrong7@my.apsu.edu>
2019-01-26 11:22:49 -0600

    About Activity update

--- app/src/main/java/com/oc/assignment_1/MainActivity.java
+++ app/src/main/java/com/oc/assignment_1/MainActivity.java
@@ -15,6 +15,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity implements View.OnClickListener{
 
@@ -22,6 +23,9 @@ public class MainActivity extends Activity implements View.OnClickListener{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button more_btn = findViewById(R.id.more_btn);
+        more_btn.setOnClickListener(this);
+
     }
 
     @Override
@@ -34,6 +38,11 @@ public class MainActivity extends Activity implements View.OnClickListener{
 
 
     }
+    public void about(View view)
+    {
+        Intent i = new Intent(getApplicationContext(),AboutActivity.class);
+        startActivity(i);
+    }
 
-   
+
 }
