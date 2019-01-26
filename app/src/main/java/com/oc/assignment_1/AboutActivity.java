973e81c10b7e8acd8022faaddba92207ee4058f2
Larmstrong1626 <rarmstrong7@my.apsu.edu>
2019-01-26 11:26:45 -0600

    Added comments to About activity, authors, etc.

--- app/src/main/java/com/oc/assignment_1/AboutActivity.java
+++ app/src/main/java/com/oc/assignment_1/AboutActivity.java
@@ -1,3 +1,14 @@
+/*************
+ Developer: Robert Armstrong/Justin Duff
+ App: Assignment_1
+ MainActivity.java
+ Tested on Samsung Galaxy 5
+ 5.1"x2.85"
+ Marshmallow 6.0.1
+ API 23
+
+ **************/
+
 package com.oc.assignment_1;
 
 import android.app.Activity;
