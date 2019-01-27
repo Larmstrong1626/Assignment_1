3a043b33268585a11eb0532496fed711a9b944f9
Larmstrong1626 <rarmstrong7@my.apsu.edu>
2019-01-27 13:53:54 -0600

    Added to notatko activity

--- app/src/main/java/com/oc/assignment_1/NotaktoActivity.java
+++ app/src/main/java/com/oc/assignment_1/NotaktoActivity.java
@@ -11,7 +11,9 @@ import android.widget.Toast;
 
 public class NotaktoActivity extends Activity {
 
-    boolean isOne, isTwo;
+    boolean isOne = true;
+    boolean isTwo;
+
     boolean OnesTurn = true;
     boolean TwosTurn = false;
     int count = 0;
@@ -29,7 +31,7 @@ public class NotaktoActivity extends Activity {
         b = new ImageButton[3][3];
 
 //row1
-        b[0][0] = findViewById(R.id.one);
+        b[0][0] = (ImageButton)findViewById(R.id.one);
         b[0][1] = findViewById(R.id.two);
         b[0][2] = findViewById(R.id.three);
 //row2
@@ -55,7 +57,7 @@ public class NotaktoActivity extends Activity {
         int x, y;
 
         //Class Constructor
-        MyListener(int x, int y) {
+        public MyListener(int x, int y) {
 
 
             this.x = x;
@@ -69,262 +71,25 @@ public class NotaktoActivity extends Activity {
             TextView my_turn = findViewById(R.id.my_turn);
             int winner_id;
 
-            if (isOne) {
-                for (x = 0; x <= 2; x++) {
-                    for (y = 0; y <= 2; y++) {
-                        if (b[x][y].isPressed()) {
-                            b[x][y].setEnabled(false);
-                            b[x][y].setBackgroundResource(R.drawable.xkey);
-                            choice[x][y] = 1;
-                            if (OnesTurn == true) {
-                                OnesTurn = false;
-                                TwosTurn = true;
-                                my_turn.setText("Player Two is up");
-                                winner_id = checkWin();
-                                if (winner_id == 2) {
-                                    Toast.makeText(NotaktoActivity.this, "Player One Wins, now back to the main menu", Toast.LENGTH_LONG).show();
+           for (x = 0; x <= 2; x++) {
+                for (y = 0; y <= 2; y++) {
+                if (b[x][y].isPressed()) {
+                       b[x][y].setEnabled(false);
+                       b[x][y].setBackgroundResource(R.drawable.x_key);
 
-                                    back();
-                                }
-                            } else {
-                                OnesTurn = true;
-                                TwosTurn = false;
-                                my_turn.setText("Player One is up");
-                                winner_id = checkWin();
-                                if (winner_id == 2) {
-                                    Toast.makeText(NotaktoActivity.this, "Player One Wins, now back to the main menu", Toast.LENGTH_LONG).show();
-
-                                    back();
-                                }
-                            }
-
-
-                        }
                     }
                 }
+
+
             }
 
 
         }
     }
-
-    public int checkWin() {
-
-
-        int row = 0;
-        int column = 0;
-        //Reverse Diagonal Check
-        for (row = 4; row >= 0; row--) {
-
-            //test_background=a[row][column].getBackground();
-            if (choice[row][column] == 2) {
-                count++;
-
-            } else {
-                count = 0;
-
-            }
-            if (count >= 5) {
-                //Toast.makeText(GameActivity.this, "Winner test works", Toast.LENGTH_LONG).show();
-                return 2;
-                //back();
-            }
-            column++;
-
-        }
-        column = 0;
-        for (row = 5; row > 0; row--) {
-
-            //test_background=a[row][column].getBackground();
-            if (choice[row][column] == 2) {
-                count++;
-
-            } else {
-                count = 0;
-
-            }
-            if (count >= 5) {
-                //Toast.makeText(GameActivity.this, "Winner test works", Toast.LENGTH_LONG).show();
-                return 2;
-                //back();
-            }
-            column++;
-
-        }
-        column = 1;
-        for (row = 5; row > 0; row--) {
-
-            //test_background=a[row][column].getBackground();
-            if (choice[row][column] == 2) {
-                count++;
-
-            } else {
-                count = 0;
-
-            }
-            if (count >= 5) {
-                //Toast.makeText(GameActivity.this, "Winner test works", Toast.LENGTH_LONG).show();
-                return 2;
-                //back();
-            }
-            column++;
-
-        }
-        column = 1;
-        for (row = 4; row >= 0; row--) {
-
-            //test_background=a[row][column].getBackground();
-            if (choice[row][column] == 2) {
-                count++;
-
-            } else {
-                count = 0;
-
-            }
-            if (count >= 5) {
-                //Toast.makeText(GameActivity.this, "Winner test works", Toast.LENGTH_LONG).show();
-                return 2;
-                //back();
-            }
-            column++;
-
-        }
-        // Diagonal check
-        for (row = 0; row < 5; row++) {
-
-
-            if (choice[row][row] == 2) {
-                count++;
-
-            } else {
-                count = 0;
-
-            }
-            if (count >= 5) {
-                //Toast.makeText(GameActivity.this, "Winner test works", Toast.LENGTH_LONG).show();
-                return 2;
-                //back();
-            }
-
-        }
-        column = 1;
-        for (row = 0; row < 5; row++) {
-
-            //test_background=a[row][column].getBackground();
-            if (choice[row][column] == 2) {
-                count++;
-
-            } else {
-                count = 0;
-
-            }
-            if (count >= 5) {
-                //Toast.makeText(GameActivity.this, "Winner test works", Toast.LENGTH_LONG).show();
-                return 2;
-                //back();
-            }
-            column++;
-
-        }
-        column = 0;
-        for (row = 1; row < 6; row++) {
-
-            //test_background=a[row][column].getBackground();
-            if (choice[row][column] == 2) {
-                count++;
-
-            } else {
-                count = 0;
-
-            }
-            if (count >= 5) {
-                //Toast.makeText(GameActivity.this, "Winner test works", Toast.LENGTH_LONG).show();
-                return 2;
-                //back();
-            }
-            column++;
-
-        }
-        column = 1;
-        for (row = 1; row < 6; row++) {
-
-            //test_background=a[row][column].getBackground();
-            if (choice[row][column] == 2) {
-                count++;
-
-            } else {
-                count = 0;
-
-            }
-            if (count >= 5) {
-                //Toast.makeText(GameActivity.this, "Winner test works", Toast.LENGTH_LONG).show();
-                return 2;
-                //back();
-            }
-            column++;
-
-        }
-
-//Chaos win
-        for (row = 0; row < 6; row++) {
-            for (column = 0; column < 6; column++) {
-                if (choice[row][column] == 1 || choice[row][column] == 2) {
-                    count++;
-                } else {
-                    count = 0;
-                }
-                if (count == 36) {
-                    Toast.makeText(NotaktoActivity.this, "Chaos wins!", Toast.LENGTH_LONG).show();
-                    back();
-                }
-            }
-        }
-        count = 0;
-// Horizontal check
-        for (row = 0; row < 6; row++) {
+        public void back() {
             count = 0;
-            for (column = 0; column < 6; column++) {
-                //test_background=a[row][column].getBackground();
-                if (choice[row][column] == 2) {
-                    count++;
-
-                } else {
-                    count = 0;
-
-                }
-                if (count >= 5) {
-                    //Toast.makeText(GameActivity.this, "Winner test works", Toast.LENGTH_LONG).show();
-                    return 2;
-                    //back();
-                }
-            }
+            Intent i = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(i);
         }
-        // Vertical check
-        for (column = 0; column < 6; column++) {
-            count = 0;
-            for (row = 0; row < 6; row++) {
-                //test_background=a[row][column].getBackground();
-                if (choice[row][column] == 2) {
-                    count++;
-
-                } else {
-                    count = 0;
-
-                }
-                if (count >= 5) {
-                    //Toast.makeText(GameActivity.this, "Winner test works", Toast.LENGTH_LONG).show();
-                    return 2;
-                    //back();
-                }
-            }
-        }
-
-        return 0;
     }
 
-    public void back() {
-        count = 0;
-        Intent i = new Intent(getApplicationContext(), MainActivity.class);
-        startActivity(i);
-    }
-}
